3b7a913f394a00db6fc2cd3dfbab036da9b5e26c
lhiqwj173 <lhssqtz@gmail.com>
2024-06-14 05:25:57 +0800

    fix

--- tg.py
+++ tg.py
@@ -129,8 +129,8 @@ def write_daily(_wait_write, id):
             # 发送到tg频道
             compress_date_file_to_tg(date)
 
-        # log(f"测试打包每日数据")
-        # compress_date_file_to_tg(date)
+        log(f"测试打包每日数据")
+        compress_date_file_to_tg(date)
 
         file = os.path.join(daily_folder, f'{date}_depth_{id}.csv')
         # 如果文件不存在，需要写入列名
