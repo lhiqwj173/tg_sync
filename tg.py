efaf5641d3009e1141a4bf54dde1c78f86fa6c0e
lhiqwj173 <lhssqtz@gmail.com>
2024-07-11 06:07:27 +0800

    debug

--- tg.py
+++ tg.py
@@ -289,7 +289,7 @@ async def sender():
             cur_t = time.time()
             if not is_done_file(file) and mtime < cur_t - 60 and ctime < cur_t - 60: 
                 # 如果有新文件，修改时间为1min前，上传到频道
-                log(f"Uploading{file}")
+                log(f"Uploading {file}")
                 # await client.send_file(entity, _file, progress_callback=progress_cb)
 
                 with open(_file, "rb") as out:
